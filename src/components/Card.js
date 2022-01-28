763f230a62b22960a76dcca677358b98b49846fb
Ryota <ryoenoki2013@gmail.com>
2022-01-29 01:20:16 +0900

    card styling: location text

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,10 @@ export default function Card(props) {
     <div className="card">
       <img src={props.item.imageUrl} alt={props.item.title} />
       <div className="card-content">
-        <p className="location-text"><span className="red"><i className="fas fa-map-marker-alt"></i></span>{props.item.location.toUpperCase()}</p>
-        <span><a href={props.item.googleMapsUrl} target="_blank" rel="noopener noreferrer">View on Google Maps</a></span>
+        <div className="card-location">
+          <p className="location-text"><span className="red"><i className="fas fa-map-marker-alt"></i></span>{props.item.location.toUpperCase()}</p>
+          <span className="map-text-gray"><a href={props.item.googleMapsUrl} target="_blank" rel="noopener noreferrer">View on Google Maps</a></span>
+        </div>
         <h3>{props.item.title}</h3>
         <p>{props.item.startDate} - {props.item.endDate}</p>
         <p>{props.item.description}</p>
