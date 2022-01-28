c9a0e2b50355c4de86942c847923547b4ea31ce6
Ryota <ryoenoki2013@gmail.com>
2022-01-28 18:53:12 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import destinationsData from "./data";
 import Header from "./components/Header";
 import Card from "./components/Card";
 
+
 function App() {
   const destinationElements = destinationsData.map(item => {
     return (
-      console.log(item.imageUrl),
       <Card
       key={item.id}
       item={item}
