30ddd2c733dbff61c735a4d274d6b527713246c3
Ryota <ryoenoki2013@gmail.com>
2022-01-31 19:31:46 +0900

    added 2 more destinations

--- src/data.js
+++ src/data.js
@@ -28,6 +28,26 @@ const destinationsData = [
     endDate: "18 Nov, 2021",
     description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
     imageUrl: "https://images.unsplash.com/photo-1601439678777-b2b3c56fa627?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+  },
+  {
+    id: 4,
+    title: "Salar de Uyuni",
+    location: "Bolivia",
+    googleMapsUrl: "https://goo.gl/maps/rK6j4ohiG5qm8u998",
+    startDate: "01 Dec, 2021",
+    endDate: "30 Dec, 2021",
+    description: "Salar de Uyuni is a mountain range in the western Bolivian Amazon. It is located in the department of Potosí, in the western Bolivian Amazon.",
+    imageUrl: "https://images.unsplash.com/photo-1526029655228-b7ee496c7819?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+  },
+  {
+    id: 5,
+    title: "Yellowknife",
+    location: "Canada",
+    googleMapsUrl: "https://goo.gl/maps/ucNV1e6qxqzAxvp36",
+    startDate: "07 Jan, 2022",
+    endDate: "26 Jan, 2022",
+    description: "Yellowknife is a city in the Canadian province of Northwest Territories. It is the capital of the Canadian province of Northwest Territories.",
+    imageUrl: "https://images.unsplash.com/photo-1634471822335-cefa9ba05277?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
   }
 ]
 
